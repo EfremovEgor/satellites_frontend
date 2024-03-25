26e9952986e9b6a994b4f211a34c77bf4e86edc5
Povid1o <93076777+Povid1o@users.noreply.github.com>
2024-03-26 01:05:28 +0300

    Earth Controls
    -fixed controls

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import CustomEarth from "./components/CustomEarth";
 
 function App() {
--- src/components/CustomEarth.js
+++ src/components/CustomEarth.js
@@ -2,23 +2,23 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 
+const rotateModel = (model) => {
+  model.rotation.y = Math.PI;
+  return model;
+};
+
 const CustomEarth = () => {
   const customModel = useGLTF("/models/earth/scene.gltf");
+  const earthModel = rotateModel(customModel.scene);
 
   return (
     <Canvas
       className="cursor-pointer"
       frameloop="demand"
-      camera={{ position: [-4, 3, 6], fov: 45, near: 0.1, far: 200 }}
+      camera={{ position: [0, 10, 20], fov: 45, near: 0.1, far: 200 }}
     >
-      <OrbitControls
-        autoRotate
-        enableZoom={false}
-        maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 2}
-        enablePan={false}
-      />
-      <primitive object={customModel.scene} scale={2.5} />
+      <OrbitControls autoRotate enableZoom={true} enablePan={true} />
+      <primitive object={earthModel} scale={5} />
     </Canvas>
   );
 };
